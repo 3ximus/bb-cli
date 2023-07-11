416f842af2ce18f4654cbf9156809cade9d3cc27
eximus <fabio4335@gmail.com>
2023-07-11 10:36:29 -0400

    remove spinners

--- api/api.go
+++ api/api.go
@@ -3,15 +3,12 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/briandowns/spinner"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
-	"time"
 )
 
 type PaginatedResponse[T any] struct {
@@ -23,7 +20,7 @@ type PaginatedResponse[T any] struct {
 	Previous string `json:"previous"`
 }
 
-func get(endpoint string, spinner *spinner.Spinner) []byte {
+func api_get(endpoint string) []byte {
 	client := &http.Client{}
 	url := fmt.Sprintf("%s/%s", viper.GetString("api"), endpoint)
 	// fmt.Println(url)
@@ -36,7 +33,6 @@ func get(endpoint string, spinner *spinner.Spinner) []byte {
 
 	if resp.StatusCode != 200 {
 		errBody, err := ioutil.ReadAll(resp.Body)
-		spinner.Stop()
 		cobra.CheckErr(err)
 		cobra.CheckErr(string(errBody))
 	}
@@ -47,25 +43,10 @@ func get(endpoint string, spinner *spinner.Spinner) []byte {
 	return body
 }
 
-// api get request wrapper with a loading spinner
-func api_get(endpoint string, loadingMessage string) []byte {
-	s := spinner.New(
-		spinner.CharSets[14],
-		100*time.Millisecond,
-		spinner.WithWriter(os.Stderr),
-		spinner.WithSuffix(loadingMessage),
-		spinner.WithColor("fgHiBlue"),
-	)
-	s.Start()
-	response := get(endpoint, s)
-	s.Stop()
-	return response
-}
-
 // HIGH LEVEL GET METHODS
 
 func GetUser() User {
-	response := api_get("user", " Fetching User...")
+	response := api_get("user")
 
 	// decode response
 	var user User
@@ -95,16 +76,13 @@ func GetPrList(repository string, state string, author string, search string, pa
 		var response []byte
 		// fmt.Printf("%v\n", prevResponse)
 		if i == 0 {
-			response = api_get(
-				fmt.Sprintf("repositories/%s/pullrequests?q=%s", repository, url.QueryEscape(stateQuery+authorQuery+searchQuery+"")),
-				fmt.Sprintf(" Fetching Pull requests (page %d/%d)...", i+1, pages),
-			)
+			response = api_get(fmt.Sprintf("repositories/%s/pullrequests?q=%s", repository, url.QueryEscape(stateQuery+authorQuery+searchQuery+"")))
 		} else {
 			newUrl := strings.Replace(prevResponse.Next, "https://api.bitbucket.org/2.0/", "", 1)
 			if newUrl == "" {
 				break // there's no next page
 			}
-			response = api_get(newUrl, fmt.Sprintf(" Fetching Pull requests (page %d/%d)...", i+1, pages))
+			response = api_get(newUrl)
 		}
 		err := json.Unmarshal(response, &prevResponse)
 		cobra.CheckErr(err)
@@ -119,9 +97,7 @@ func GetPr(repository string, id int) <-chan PullRequest {
 	go func() {
 		defer close(channel)
 		var pr PullRequest
-		response := api_get(
-			fmt.Sprintf("repositories/%s/pullrequests/%d", repository, id),
-			" Fetching Pull request...")
+		response := api_get(fmt.Sprintf("repositories/%s/pullrequests/%d", repository, id))
 		err := json.Unmarshal(response, &pr)
 		cobra.CheckErr(err)
 		channel <- pr
@@ -134,9 +110,7 @@ func GetPrStatuses(repository string, id int) <-chan []CommitStatus {
 	go func() {
 		defer close(channel)
 		var paginatedResponse PaginatedResponse[CommitStatus]
-		response := api_get(
-			fmt.Sprintf("repositories/%s/pullrequests/%d/statuses", repository, id),
-			" Fetching Statuses...")
+		response := api_get(fmt.Sprintf("repositories/%s/pullrequests/%d/statuses", repository, id))
 		err := json.Unmarshal(response, &paginatedResponse)
 		cobra.CheckErr(err)
 		channel <- paginatedResponse.Values
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module bb
 go 1.20
 
 require (
-	github.com/briandowns/spinner v1.23.0
 	github.com/ldez/go-git-cmd-wrapper/v2 v2.6.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
@@ -11,13 +10,10 @@ require (
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
@@ -27,7 +23,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/term v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
