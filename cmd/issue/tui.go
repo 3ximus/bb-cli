efb8148a870a743886cebf29ded895bb4ccfac0a
eximus <fabio4335@gmail.com>
2024-05-05 18:22:26 -0400

    issue tui - multiple improvements to list presentation

--- cmd/issue/tui.go
+++ cmd/issue/tui.go
@@ -2,34 +2,24 @@ package issue
 
 import (
 	"bb/api"
+	"bb/util"
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-// create global styles
-var (
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(2)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(0).Foreground(lipgloss.Color("3"))
-)
-
-var listingChannel <-chan api.JiraIssue
-
-type newDataLoaded struct {
-	data []list.Item
-}
+// ====================================================
+//                     ITEMS
+// ====================================================
 
 // Defines the structure of each item in a list
 type item api.JiraIssue
 
-func (i item) Title() string       { return i.Key }
-func (i item) Description() string { return i.Fields.Summary }
-func (i item) FilterValue() string { return i.Key }
+func (i item) FilterValue() string { return i.Key + i.Fields.Summary }
 
 // List item delegate defines how to render a list
 type itemDelegate struct{}
@@ -38,39 +28,67 @@ func (d itemDelegate) Height() int                             { return 1 }
 func (d itemDelegate) Spacing() int                            { return 0 }
 func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
 func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
+	i, _ := listItem.(item) // convert to api.JiraIssue
+	fmtStr := "  %s \033[1;32m%s\033[m %s %s %s"
+	if index == m.Index() { // selected index
+		fmtStr = "\033[1;35m| %s \033[1;35m%s %s \033[1;35m%s %s"
 	}
-
-	str := fmt.Sprintf("%s", i.Key)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("| " + strings.Join(s, ""))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
+	fmt.Fprintf(w, fmtStr,
+		util.FormatIssueStatus(i.Fields.Status.Name),
+		i.Key,
+		util.FormatIssueType(i.Fields.Type.Name),
+		i.Fields.Summary,
+		util.FormatIssuePriority(i.Fields.Priority.Id, i.Fields.Priority.Name))
 }
 
+// ====================================================
+//                     MESSAGES
+// ====================================================
+
+type started bool
+type issueList struct {
+	data []list.Item
+}
+
+func getIssueList() tea.Msg {
+	items := []list.Item{}
+	for issue := range api.GetIssueList(100, false, false, "", []string{}, []string{}, "", false) {
+		items = append(items, item(issue))
+	}
+	return issueList{items}
+}
+
+// Function that will just trigger the startup update
+// turning on the spinner
+func startup() tea.Msg {
+	return started(true)
+}
+
+// ====================================================
+//                   TUI METHODS
+// ====================================================
+
+// The main TUI model
 type model struct {
-	list       list.Model
-	data       []api.JiraIssue
-	dataLoaded bool
+	list list.Model
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(startup, getIssueList)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case started:
+		return m, m.list.StartSpinner()
 	case tea.WindowSizeMsg:
 		m.list.SetWidth(msg.Width)
 		return m, nil
-
+	case issueList:
+		return m, tea.Batch(
+			m.list.SetItems(msg.data),
+			m.list.ToggleSpinner(),
+		)
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
 		case "q", "ctrl+c":
@@ -85,8 +103,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				)
 			}
 		}
-	case newDataLoaded:
-		return m, m.list.SetItems(msg.data)
 	}
 
 	var cmd tea.Cmd
@@ -98,27 +114,20 @@ func (m model) View() string {
 	return m.list.View()
 }
 
+// Main Function
 func TUI() {
-	listingChannel = api.GetIssueList(100, false, false, "", []string{}, []string{}, "", false)
-
 	l := list.New([]list.Item{}, itemDelegate{}, 0, 13)
-	l.Title = "Issue list:"
+	l.Title = "Jira issue list:"
+	l.InfiniteScrolling = true
 	l.Styles.Title = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.ANSIColor(4))
 	l.SetShowStatusBar(false)
+	// TODO
+	// l.Styles.Spinner = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.ANSIColor(1))
 	l.Styles.HelpStyle = list.DefaultStyles().HelpStyle.PaddingLeft(2).PaddingTop(0)
 
-	m := model{list: l, dataLoaded: false}
+	m := model{list: l}
 
 	p := tea.NewProgram(m)
-
-	// load data async into the list
-	go func() {
-		items := []list.Item{}
-		for issue := range listingChannel {
-			items = append(items, item{Key: issue.Key})
-		}
-		p.Send(newDataLoaded{items})
-	}()
 
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Error running program:", err)
